8ffa7512cf9a39012fa092c0e264f587ab082c5f
CodingRatio <coding.project1.618@gmail.com>
2024-01-18 00:34:31 -0600

    Add style to components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,11 +1,20 @@
 export default function Contact(){
     return(
         <div className="contact--container">
-            <ul className="contact--list--container">
-                <li>Book an appointment online!</li>
-                <li>(832) 219 - 1348 </li>
-                <li>realemail@email.dev</li>
-            </ul>
+            <section className="contact--list--container">
+                <div className="link--container">
+                    <a href="#" className="list--icon"> <img src="src/assets/appointment.png"/></a>
+                    <a href="#" className="list--text"><span>Book an appointment online!</span></a>
+                </div>
+                <div className="link--container">
+                    <a href="#" className="list--icon"><img src="src/assets/telephone-call.png"/></a>
+                    <a href="#" className="list--text"><span>(832) 219 - 1438</span></a>
+                </div>
+                <div className="link--container">
+                    <a href="#" className="list--icon"><img src="src/assets/mail.png"/></a>
+                    <a href="#" className="list--text"><span>realemail@email.dev</span></a>
+                </div>
+            </section>
         </div>
     )
 }
